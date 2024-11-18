a49144194f1799ef439a981b8ca44cbc9c6b6314
EduardoRN99 <eduardo.ribeiro99@live.com>
2024-11-18 14:29:14 -0300

    Update Cliente.cs

--- Cliente.cs
+++ Cliente.cs
@@ -1,16 +1,18 @@
-﻿using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace AfroditeClasses.Models
 {
     public class Cliente : Pessoa
     {
+        [Key]
         public int IdCliente { get; set; }
 
-        public Cliente(string nome, string telefone, string email, string senha, int idCliente)
+        // Construtor que inicializa com parâmetros
+        public Cliente(string nome, string telefone, string email, string senha)
             : base(nome, telefone, email, senha)
         {
-            IdCliente = idCliente;
+            IdCliente = IdCliente;
         }
     }
 }
